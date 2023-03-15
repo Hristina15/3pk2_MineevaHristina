4f256e108e84c02aaefda1419c560bf5da09d42d
hristinamineeva27@gmail.com <hristinamineeva27@gmail.com>
2023-03-15 22:41:16 +0500

    =

---
+++ ПЗ_5/Cat.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ПЗ_5
+{
+
+    enum Paroda
+    {
+        Siamskaya,
+        Britanskaya,
+        Sfinks
+    }
+
+    internal class Cat : ICloneable, IComparable<Cat>
+    {
+
+        private string _name;
+        private int _yo;
+        private Paroda _paroda;
+
+        public Cat(string name, int yo, Paroda paroda)
+        {
+            _name = name;
+            _yo = yo;
+            _paroda = paroda;
+        }
+
+        public string NAME
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public int YO
+        {
+            get
+            {
+                return _yo;
+            }
+            set
+            {
+                _yo = value;
+            }
+        }
+
+        public Paroda PARODA
+        {
+            get
+            {
+                return _paroda;
+            }
+            set
+            {
+                _paroda = value;
+            }
+        }
+
+        public object Clone()
+        {
+            return new Cat(NAME, YO, PARODA);
+        }
+
+        public int CompareTo(Cat? other)
+        {
+            if (other is Cat cat) return NAME.CompareTo(cat.NAME);
+            else throw new ArgumentException("Некорректное значение параметра"); throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"{_name}, {_yo} лет, порода: {_paroda}";
+        }
+
+    }
+}
---
+++ ПЗ_5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection.Metadata;
+using ПЗ_5;
+
+var Tom = new Cat("Tom", 3, Paroda.Britanskaya);
+
+Console.WriteLine(Tom.ToString());
+
+var Bob = Tom;
+Bob.NAME = "Bob";
+
+Console.WriteLine(Tom.ToString());
+Console.WriteLine(Bob.ToString());
+
+
+
+var Beta = new Cat("Beta", 3, Paroda.Sfinks);
+var Barsik = (Cat)Beta.Clone();
+
+Barsik.NAME = "Barsik";
+
+Console.WriteLine(Beta.ToString());
+Console.WriteLine(Barsik.ToString());
+
