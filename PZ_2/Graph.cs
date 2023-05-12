7767c9146ce207190847fddc50acdbec4e5e1af6
hristinamineeva27@gmail.com <hristinamineeva27@gmail.com>
2023-05-12 20:16:51 +0500

    пз2

---
+++ PZ_2/Graph.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_2
+{
+    internal class Graph
+    {
+        private int[,] a; // матрица смежности графа
+
+        public Graph(int[,] a)
+        {
+            this.a = a;
+        }
+
+        // метод для проверки связности графа
+        public bool IsConnected()
+        {
+            int n = a.GetLength(0); // количество вершин в графе
+            bool[] visited = new bool[n]; // массив для отметки посещенных вершин
+            D(0, visited); // запускаем обход графа в глубину из первой вершины
+
+            // если все вершины были посещены, то граф связный
+            for (int i = 0; i < n; i++)
+            {
+                if (!visited[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // рекурсивный метод обхода графа в глубину
+        private void D(int currentVertex, bool[] visited)
+        {
+            visited[currentVertex] = true;
+            int n = a.GetLength(0);
+
+            // проходим по всем вершинам, смежным с текущей, и рекурсивно вызываем обход из них
+            for (int i = 0; i < n; i++)
+            {
+                if (a[currentVertex, i] != 0 && !visited[i])
+                {
+                    D(i, visited);
+                }
+            }
+        }
+    }
+}
---
+++ PZ_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[,]
+        {
+                {0, 1, 1, 0, 0},
+                {0, 0, 0, 1, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 1, 0}
+        };
+            Graph graph = new Graph(a);
+
+            // проверяем связность графа
+            bool isConnected = graph.IsConnected();
+            Console.WriteLine("Граф является связным: " + isConnected);
+            Console.ReadLine();
+        }
+    }
+}
