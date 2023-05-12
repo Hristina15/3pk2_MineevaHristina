cd1d456fe703283d03d7fec275a350fdab439b1f
hristinamineeva27@gmail.com <hristinamineeva27@gmail.com>
2023-05-12 21:43:24 +0500

    8

---
+++ PZ_8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Иван Иванов", "123-45-67", "ул. Пушкина, д. 10");
+            School school = new School("Школа №1", "234-56-78", "ул. Лермонтова, д. 5", "Петров П.П.");
+
+            // создаем телефонную книгу и добавляем в нее контакты
+            PhoneBook phoneBook = new PhoneBook();
+            phoneBook.AddContact(person);
+            phoneBook.AddContact(school);
+
+            // отображаем все контакты в телефонной книге
+            phoneBook.DisplayContacts();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PZ_8/School.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PZ_8
+{
+    internal class School : ContactObject
+    {
+        public string Address { get; set; } // адрес
+        public string Director { get; set; } // директор
+
+        public School(string name, string phone, string address, string director) : base(name, phone)
+        {
+            Address = address;
+            Director = director;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("Образовательное учреждение");
+            Console.WriteLine("Название: " + Name);
+            Console.WriteLine("Телефон: " + Phone);
+            Console.WriteLine("Адрес: " + Address);
+            Console.WriteLine("Директор: " + Director);
+        }
+    }
+}
